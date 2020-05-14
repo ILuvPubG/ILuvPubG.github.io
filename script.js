b43beede9302dac6ced6336ad6b3b4964533af42
ILuvPubG <65266597+ILuvPubG@users.noreply.github.com>
2020-05-14 16:12:05 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
-for (var i = 0; i < 10; i++) {
+var num = 20;
+
+for (var i = 0; i < num; i++) {
    var n = document.createElement("div");
    n.classList.add("box");
-   var size = Math.floor(Math.random()*80)+20;
-   n.style.left = (Math.random() * 100)+"%";
+   var size = Math.floor(Math.random()*100)+100;
+   n.style.left = ((i*(100/num))+(100/(2*num)))+"%";
    n.style.animationDelay = (Math.random() * 5000)+"ms";
    n.style.animationDuration = (Math.random() * 2000)+4000+"ms";
    n.style.width = size+"px";
    n.style.height = size+"px";
+   n.style.background = 'url("img/test.png")';
+   n.style.backgroundSize = "contain";
    document.body.appendChild(n);
 }
